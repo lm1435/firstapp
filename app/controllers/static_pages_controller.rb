f6f36c22bd85bb2b6458a4d78505c7dd028d91f2
Luis Meza <luismeza01@gmail.com>
2016-08-30 17:26:01 -0500

    added carousel and partials

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,8 +7,10 @@ end
 class StaticPagesController < ApplicationController
 
 	def landing_page
-		@featured_product = Product.third
+		@featured_product = Product.first
+		@products = Product.limit(4)
 	end
 
 end
+
 
