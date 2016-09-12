cc63e5b4e692f0f745bffa899c4ab32b186d3aab
Luis Meza <luismeza01@gmail.com>
2016-09-11 21:24:14 -0500

    5.3 removing ActionMailer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,18 +4,12 @@ class StaticPagesController < ApplicationController
 
 	def landing_page
 		@featured_product = Product.first
-		@products = Product.limit(4)
-	end
 
 	def thank_you
 		@name = params[:name]
 		@email = params[:email]
 		@message = params[:message]
 		UserMailer.contact_form(@email, @name, @message).deliver_now
-		ActionMailer::Base.mail(:from => @email,
-			:to => 'austinbikesruby@gmail.com',
-	 		:subject => "A new contact form message from #{@name}",
-			:body => @message).deliver_now
 	end
 
 end
