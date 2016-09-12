bead620a822f58c1d73de8b07a2d1722983d1918
Luis Meza <luismeza01@gmail.com>
2016-09-11 20:12:36 -0500

    5.3 edited mailer file for testing.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,12 +11,12 @@ class StaticPagesController < ApplicationController
 		@name = params[:name]
 		@email = params[:email]
 		@message = params[:message]
-		UserMailer.contact_form(@email, @name, @message).deliver_now
-		# ActionMailer::Base.mail(
-		# 	:from => @email,
-		# 	:to => 'austinbikesruby@gmail.com',
-		# 	:subject => 'A new contact form message sent from #{@name}',
-		# 	:body => @message).deliver_now
+		# UserMailer.contact_form(@email, @name, @message).deliver_now
+		ActionMailer::Base.mail(
+			:from => @email,
+			:to => 'austinbikesruby@gmail.com',
+			:subject => 'A new contact form message sent from #{@name}',
+			:body => @message).deliver_now
 
 	end
 
