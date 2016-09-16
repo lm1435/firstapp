d9dabbcb427c7d5df4a44079033006bd5538e29f
Luis Meza <luismeza01@gmail.com>
2016-09-15 21:00:17 -0500

    5.5 cancancan and alerts

--- Gemfile
+++ Gemfile
@@ -52,3 +52,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise'
 
+gem 'cancancan', '~> 1.10'
+
+
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, :except => [:show, :index]
+  before_filter :authenticate_user!
+  load_and_authorize_resource
 
   # GET /users
   # GET /users.json
@@ -62,6 +65,7 @@ class UsersController < ApplicationController
   end
 
   private
+  
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
