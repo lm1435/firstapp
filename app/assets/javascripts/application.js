b9369419a327748d63a0cf6dc347c3bbfe728be3
Luis Meza <luismeza01@gmail.com>
2016-09-23 17:44:56 -0500

    5.8 fix raty issue and add js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,4 +17,10 @@
 
 $(document).on('turbolinks:load', function(){
     $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+    $('.rated').raty({path: '/assets', 
+		readOnly: true, 
+		score: function(){
+			return $(this).attr('data-score');
+		}
+	});
 });
