4e517e04ec800673f1f1fccc30dbc2789abb8d66
Luis Meza <luismeza01@gmail.com>
2016-09-27 20:49:51 -0500

    5.10 added admin usability and new feature link_to img

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,16 +11,20 @@ class CommentsController < ApplicationController
 				format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
 				format.json { render json: @comment.errors, status: :unprocessable_entity }
 			end
-		end	
-	end 
+		end
+	end
 
 	def destroy
+		@comment = comment.find(params[:id])
+		product = @comment.product
+		@comment.destroy
+		redirect_to product
 	end
 
 
 
 	private
-	
+
 		def	comment_params
 			params.require(:comment).permit(:user_id, :body, :rating)
 		end
