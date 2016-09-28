db573ac766c3fc11607251e7c502974f9fbaaabf
Luis Meza <luismeza01@gmail.com>
2016-09-27 21:19:37 -0500

    modifying controller to use correct method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,7 +15,7 @@ class CommentsController < ApplicationController
 	end
 
 	def destroy
-		@comment = comment.find(params[:id])
+		@comment = Comment.find(params[:id])
 		product = @comment.product
 		@comment.destroy
 		redirect_to product
