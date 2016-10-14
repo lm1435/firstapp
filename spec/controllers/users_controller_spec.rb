d0d8b587553463b03661d3edf35cafa099cf3ad3
Luis Meza <luismeza01@gmail.com>
2016-10-13 19:33:52 -0500

    added correct testing code

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,28 +2,28 @@ require 'rails_helper'
 
 describe UsersController, :type => :controller do
   before do
-    let(:user) {User.create!(first_name: 'exa', last_name: 'mple', email: 'exa@example.com', password: '1234321')}
+    @user=User.create!(first_name: 'exa', last_name: 'mple', email: 'exa@example.com', password: '1234321')
 
-    let(:user1) {User.create!(first_name: 'seco', last_name: 'nd',email: 'seco@second.com', password: '4321234')}
+    @user1=User.create!(first_name: 'seco', last_name: 'nd',email: 'seco@second.com', password: '4321234')
+  end
 
-
-    describe 'GET #show' do
-      context 'user is logged in' do
-        before do
-          sign_in user
-        end
-
-        it 'loads correct user details' do
-          expect(response).to have_http_status(200)
-          expect(assigns(:user)).to eq user
-        end
+  describe 'GET #show' do
+    context 'user is logged in' do
+      before do
+        sign_in @user
       end
 
-      context 'No user is logged in' do
-        it 'redirects to login' do
-          get :show, id: user.id
-          expect(response).to redirect_to(root_path)
-        end
+      it 'loads correct user details' do
+        get :show, id: @user.id
+        expect(response).to have_http_status(200)
+        expect(assigns(:user)).to eq @user
+      end
+    end
+
+    context 'No user is logged in' do
+      it 'redirects to login' do
+        get :show, id: @user.id
+        expect(response).to redirect_to(new_user_session_path)
       end
     end
   end
